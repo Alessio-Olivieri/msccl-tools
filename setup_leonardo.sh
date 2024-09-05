8b341b1742737344b44da48aae4691fe044ae562
Alessio-Olivieri <lexyo.2002@gmial.com>
2024-09-05 11:25:54 +0200

    setup leonardo

---
+++ setup_leonardo.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+# salloc -N 1 -n 4 --gres=gpu:4 --time=02:00:00 --exclusive --account=IscrC_SHARP_0 -p boost_usr_prod 
+module load cuda
+module load openmpi
+export NVCC_LOCATION=$(which nvcc)
