2934bfa442dcf53cfb38219acb57be78d82acdd0
Alessio-Olivieri <lexyo.2002@gmial.com>
2024-09-17 17:19:22 +0200

    initial commit

--- examples/mscclang/1allreduce_latency_optimal_swing.py
+++ examples/mscclang/1allreduce_latency_optimal_swing.py
@@ -11,17 +11,48 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def send_whole_buffer(source_rank, dest_rank):
-    # Reference the source buffer (input buffer in this case)
-    src_buffer = Buffer.input
+def pi(r, s, n):
+    p = (1 - math.pow(-2, s+1))/3
+    peer = (r + p) % n if r % 2 == 0 else (r - p) % n
+    return int(peer)
 
-    # Initialize the chunk index
-    chunk_index = 0
-    
-    c_source = chunk(source_rank, Buffer.input, 0, 4)
-    c_dest = chunk(dest_rank, Buffer.input, 0, 4)
-    c_dest.reduce(c_source)
+def log2_allreduce_swing(size, buffer_size):
+    # run swing on log2 ranks
+    for step in range(int(math.log2(size))):
+        logger.debug(f"Starting step {step}")
+        # Each rank sends the nth chunk to the nth rank into scratch space
+        for r in range(size):
+            peer = pi(r, step, size)
+            logger.debug(f"copying buffer of {r} into scratch of {peer}")
+            c = chunk(r, Buffer.input, index=0, size=buffer_size)
+            c.copy(peer, 'scratch', 0, recvtb=peer, sendtb=r)
 
+        for rank in range(size):
+            logger.debug(f"reducing scratch buffer of {rank} into input buffer")
+            c = chunk(rank, Buffer.input, 0, size=buffer_size)
+            c.reduce(chunk(rank, 'scratch', 0, size=buffer_size))
+
+def send_buffers(size, buffer_size):
+    peer = size - 1
+    for r in range(size, buffer_size):
+            logger.debug(f"copying buffer of {r} into scratch of {peer}")
+            c = chunk(r, Buffer.input, index=0, size=buffer_size)
+            c.copy(peer, 'scratch', 0, recvtb=peer, sendtb=r)
+            peer -= 1
+
+    for rank in range(size):
+        logger.debug(f"reducing scratch buffer of {rank} into input buffer")
+        c = chunk(rank, Buffer.input, 0, size=buffer_size)
+        c.reduce(chunk(rank, 'scratch', 0, size=buffer_size))
+
+def recv_buffers(size, buffer_size):
+    # The log2_size ranks send to the input buffer (because it's inplace) of the extra ranks
+    peer = size - 1
+    for r in range(size, buffer_size):
+            logger.debug(f"copying buffer of {r} into input of {peer}")
+            c = chunk(peer, Buffer.input, index=0, size=buffer_size)
+            c.copy(r, Buffer.input, 0, recvtb=r, sendtb=peer)
+            peer -= 1
 
 def allreduce_swing(size, instances):
     # Logical topology
@@ -29,41 +60,15 @@ def allreduce_swing(size, instances):
     collective = AllReduce(size, size, inplace=True)
 
     with MSCCLProgram("allreduce_swing", topology, collective, instances):
-        def pi(r, s, n):
-            p = (1 - math.pow(-2, s+1))/3
-            peer = (r + p) % n if r % 2 == 0 else (r - p) % n
-            return int(peer)
         
-        # send_whole_buffer(0,1)
+        size_log2 = 2 ** (int(math.log2(size))) #The closest power of 2 (smaller) to size
+
+
+        send_buffers(size_log2, size)# size - size_log2 ranks send to size ranks their buffers
         
-        for step in range(int(math.log2(size))):
-            logger.debug(f"Starting step {step}")
-            # Each rank sends the nth chunk to the nth rank into scratch space
-            for r in range(size):
-                peer = pi(r, step, size)
-                logger.debug(f"copying buffer of {r} into scratch of {peer}")
-                c = chunk(r, Buffer.input, index=0, size=size)
-                c.copy(peer, 'scratch', 0, recvtb=peer, sendtb=r)
+        log2_allreduce_swing(size_log2, size)# size_log2 nodes run swing
 
-            for rank in range(size):
-                logger.debug(f"reducing scratch buffer of {rank} into input buffer")
-                c = chunk(rank, Buffer.input, 0, size=size)
-                c.reduce(chunk(rank, 'scratch', 0, size=size))
-
-            
-
-
-    
-        
-        
-
-        #     # Each rank sends the nth chunk to the nth rank into scratch space
-        #     for r1 in range(size):
-        #         for r2 in range(size):
-        #             if r1 != r2:
-        #                 index = r2 * size
-        #                 c = chunk(r1, Buffer.input, index, size=size)
-        #                 c.copy(r2, 'scratch', sendtb=r2, recvtb=r1)
+        recv_buffers(size_log2, size)# size - size_log2 ranks recive from size ranks the whole buffer
 
         Check()
         XML()
