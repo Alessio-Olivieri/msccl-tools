56e37b444a38cba945f3fdfdd664dae4fede9c91
Alessio-Olivieri <lexyo.2002@gmial.com>
2024-09-26 15:15:06 +0000

    Improved performance of initialization in case of different number of n

--- examples/mscclang/1allreduce_latency_optimal_swing.py
+++ examples/mscclang/1allreduce_latency_optimal_swing.py
@@ -16,55 +16,89 @@ def pi(r, s, n):
     peer = (r + p) % n if r % 2 == 0 else (r - p) % n
     return int(peer)
 
-def allreduce_swing(size, instances):
+def allreduce_swing(size, instances, pairs):
     # Logical topology
     topology = fully_connected(size)
     collective = AllReduce(size, size, inplace=True)
 
     with MSCCLProgram("allreduce_swing", topology, collective, instances):
+        if not pairs:
+            size_log2 = 2 ** (int(math.log2(size))) #The closest power of 2 (smaller) to size
+
+            #The extra ranks send to the log2 ranks
+            peer = size_log2-1
+            for r in range(size_log2, size):
+                logger.debug(f"sending input buffer of {r} into scratch of {peer}")
+                peer_scratch = chunk(r, Buffer.input, 0, size=size).copy(peer, 'scratch', 0, sendtb=r, recvtb=peer)
+                chunk(peer, Buffer.input, 0, size=size).reduce(peer_scratch)
+                peer -= 1
+
+            # run swing on log2 ranks
+            for step in range(int(math.log2(size_log2))):
+                logger.debug(f"Starting step {step}")
+                # Each rank sends it's buffer to it's peer and the peer performs a reduce
+                for r in range(size_log2):
+                    peer = pi(r, step, size_log2)
+                    logger.debug(f"copying input buffer of {r} into scratch of {peer}")
+                    chunk(r, Buffer.input, index=0, size=size).copy(peer, 'scratch', 0, recvtb=peer, sendtb=r)
+                for r in range(size_log2):
+                    logger.debug(f"reducing scratch buffer of {r} into the input buffer of {r}")
+                    chunk(r, Buffer.input, 0, size=size).reduce(chunk(r, 'scratch', 0, size=size))
+
+            #The extra ranks recive from the log2 ranks
+            peer = size_log2-1
+            for r in range(size_log2, size):
+                chunk(peer, Buffer.input, 0, size=size).copy(r, Buffer.input, 0, recvtb=r, sendtb=peer)
         
-        size_log2 = 2 ** (int(math.log2(size))) #The closest power of 2 (smaller) to size
+        if pairs:
 
-        #The extra ranks send to the log2 ranks
-        peer = size_log2-1
-        for r in range(size_log2, size):
-            logger.debug(f"sending input buffer of {r} into scratch of {peer}")
-            peer_scratch = chunk(r, Buffer.input, 0, size=size).copy(peer, 'scratch', 0, sendtb=r, recvtb=peer)
-            chunk(peer, Buffer.input, 0, size=size).reduce(peer_scratch)
-            peer -= 1
+            size_log2 = 2 ** (int(math.log2(size))) #The closest power of 2 (smaller) to size
+            size_extra = size-size_log2
 
-        # run swing on log2 ranks
-        for step in range(int(math.log2(size_log2))):
-            logger.debug(f"Starting step {step}")
-            # Each rank sends it's buffer to it's peer and the peer performs a reduce
-            for r in range(size_log2):
-                peer = pi(r, step, size_log2)
-                logger.debug(f"copying input buffer of {r} into scratch of {peer}")
-                chunk(r, Buffer.input, index=0, size=size).copy(peer, 'scratch', 0, recvtb=peer, sendtb=r)
-            for r in range(size_log2):
-                logger.debug(f"reducing scratch buffer of {r} into the input buffer of {r}")
-                chunk(r, Buffer.input, 0, size=size).reduce(chunk(r, 'scratch', 0, size=size))
+            aliases = [] #The actual ranks that execute swing
+            siblings = [] #Log2 ranks and extra ranks pairs
 
-        #The extra ranks recive from the log2 ranks
-        peer = size_log2-1
-        for r in range(size_log2, size):
-            chunk(peer, Buffer.input, 0, size=size).copy(r, Buffer.input, 0, recvtb=r, sendtb=peer)
+            r = 0
+            while r<size:
+                if size_extra>0:
+                    aliases.append(r)
+                    siblings.append((r,r+1))
+                    r+=2
+                    size_extra-=1
+                else:
+                    aliases.append(r)
+                    r+=1
+
+            for r, extrar in siblings:
+                peer_scratch = chunk(extrar, Buffer.input, 0, size=size).copy(r, 'scratch', 0, sendtb=extrar, recvtb=r)
+                chunk(r, Buffer.input, 0, size=size).reduce(peer_scratch)
+
+            # run swing on log2 ranks
+            for step in range(int(math.log2(size_log2))):
+                logger.debug(f"Starting step {step}")
+                # Each rank sends it's buffer to it's peer and the peer performs a reduce
+                for r in range(size_log2):
+                    peer = pi(r, step, size_log2)
+                    logger.debug(f"copying input buffer of {aliases[r]} into scratch of {aliases[r]}")
+                    chunk(aliases[r], Buffer.input, index=0, size=size).copy(aliases[peer], 'scratch', 0, recvtb=aliases[peer], sendtb=aliases[r])
+                for r in range(size_log2):
+                    logger.debug(f"reducing scratch buffer of {aliases[r]} into the input buffer of {aliases[r]}")
+                    chunk(aliases[r], Buffer.input, 0, size=size).reduce(chunk(aliases[r], 'scratch', 0, size=size))
             
-
-        
-             
-
-        # log2_allreduce_swing(size_log2, size)# size_log2 nodes run swing
-
-        # recv_buffers(size_log2, size)# size - size_log2 ranks recive from size ranks the whole buffer
+            for r, extrar in siblings:
+                chunk(r, Buffer.input, 0, size=size).copy(extrar, Buffer.input, 0, recvtb=extrar, sendtb=r)
 
         XML()
-        Check()
+        Check()            
+
+
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('num_gpus', type=int, help ='number of gpus')
 parser.add_argument('instances', type=int, help='number of instances')
-
+parser.add_argument('pairs', type=bool, default=False, nargs='?')
 args = parser.parse_args()
 
-allreduce_swing(args.num_gpus, args.instances)
+allreduce_swing(args.num_gpus, args.instances, args.pairs)
